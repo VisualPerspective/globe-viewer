4c938190e1064c887a739f7312cec8fda3eefe4e
Kevin James <kevinwjames@gmail.com>
2016-06-28 18:08:06 -0400

    refactor css into scss

--- app/shaders/functions/exposure.glsl
+++ app/shaders/functions/exposure.glsl
@@ -1,9 +1,11 @@
 // Set camera exposure based on angle between sun and eye
 
-float exposure = mix(
-  2.5,
-  300.0,
-  pow((1.0 - dot(normalize(vEye), L)) / 2.0, 8.0)
-);
+float exposure(vec3 eye, vec3 L) {
+  return mix(
+    2.5,
+    300.0,
+    pow((1.0 - dot(normalize(eye), L)) / 2.0, 8.0)
+  );
+}
 
 #pragma glslify: export(exposure)

83e7f63ee09020097a8d146a07fd457afb2d5657
Kevin James <kevinwjames@gmail.com>
2016-07-19 16:12:39 -0400

    work on getting vector data

---
+++ script/make-vectors.sh
@@ -0,0 +1,32 @@
+# based on https://github.com/cambecc/earth
+mkdir -p tmpdata
+cd tmpdata
+
+# land
+if [ ! -f ne_50m_land.zip ]; then
+  curl "http://naciscdn.org/naturalearth/50m/physical/ne_50m_land.zip" -o ne_50m_land.zip
+fi
+unzip -o ne_50m_land.zip
+
+# lakes
+if [ ! -f ne_50m_lakes.zip ]; then
+  curl "http://naciscdn.org/naturalearth/50m/physical/ne_50m_lakes.zip" -o ne_50m_lakes.zip
+fi
+unzip -o ne_50m_lakes.zip
+
+# rivers
+if [ ! -f ne_50m_rivers_lake_centerlines_scale_rank.zip ]; then
+  curl "http://naciscdn.org/naturalearth/50m/physical/ne_50m_rivers_lake_centerlines_scale_rank.zip" -o ne_50m_rivers_lake_centerlines_scale_rank.zip
+fi
+unzip -o ne_50m_rivers_lake_centerlines_scale_rank.zip
+
+
+# countries
+if [ ! -f ne_50m_admin_0_countries.zip ]; then
+  curl "http://naciscdn.org/naturalearth/50m/cultural/ne_50m_admin_0_countries.zip" -o ne_50m_admin_0_countries.zip
+fi
+unzip -o ne_50m_admin_0_countries.zip
+
+topojson -o vectors.json -- land=ne_50m_land.shp lakes=ne_50m_lakes.shp rivers=ne_50m_rivers_lake_centerlines_scale_rank.shp countries=ne_50m_admin_0_countries.shp
+
+cp vectors.json ../app/assets/data/

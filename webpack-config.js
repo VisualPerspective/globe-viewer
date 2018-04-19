639b59836d1f63fba329f6948eb9023abebd449e
Kevin James <kevinwjames@gmail.com>
2018-04-19 16:14:18 -0400

    Update packages and switch to webpack

---
+++ webpack-config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: './app/initialize.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-es2016']
+          }
+        }
+      },
+      {
+        test: /\.(glsl|frag|vert)$/,
+        exclude: /node_modules/,
+        loader: 'webpack-glsl-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [{
+            loader: "css-loader"
+          }, {
+            loader: "sass-loader"
+          }]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([{ from: 'app/assets', to: '.' }]),
+    new ExtractTextPlugin('app.css')
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    modules: ['node_modules', 'app'],
+    extensions: ['.js', '.json', '.glsl']
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist/',
+    port: 3333
+  },
+};

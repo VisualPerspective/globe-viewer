a81f58ce72a36dea74b395386dbbdbe88948d98f
Kevin James <kevinwjames@gmail.com>
2019-10-23 16:25:46 -0400

    Update dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -12,7 +12,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-es2016']
+            presets: ['@babel/preset-env']
           }
         }
       },
